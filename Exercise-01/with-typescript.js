9ad3553a3306c73992e3fce0c3cd05e1197ce8ab
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-24 17:06:41 -0500

    Initial commit

---
+++ Exercise-01/with-typescript.js
@@ -0,0 +1,8 @@
+// Declare a variable studentName of type string and assign it your name.
+// Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+// Log these variables to the console.
+
+var studentName = "Amanda";
+var totalCourses = 2;
+console.log("Student Name:", studentName);
+console.log("Total Courses:", totalCourses);
---
+++ Exercise-01/with-typescript.ts
@@ -0,0 +1,11 @@
+// Declare a variable studentName of type string and assign it your name.
+// Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+// Log these variables to the console.
+
+const studentName: string = "Amanda";
+
+const totalCourses: number = 2; 
+
+
+console.log("Student Name:", studentName);
+console.log("Total Courses:", totalCourses);
