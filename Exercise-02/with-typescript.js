c5064283918511abd2cd6615afb35ea8587fbab5
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-24 17:25:57 -0500

    Completed Exercise 2
    Completed exercise 2

---
+++ Exercise-02/with-typescript.js
@@ -0,0 +1,9 @@
+// Declare an array named subjects containing strings.
+// Declare a tuple named student that contains a string (for the name) and a number (for the age).
+// Declare an array named subjects containing strings.
+var subjects = ["Math", "Science", "History", "English"];
+// Declare a tuple named student that contains a string (for the name) and a number (for the age).
+var student = ["Amanda", 34];
+
+console.log("Subjects:", subjects);
+console.log("Student:", student);
---
+++ Exercise-02/with-typescript.ts
@@ -0,0 +1,9 @@
+// Declare an array named subjects containing strings.
+// Declare a tuple named student that contains a string (for the name) and a number (for the age).
+
+const subjects: string[] = ["English", "Art", "Science", "Math"];
+
+const student: [string, number] = ["Amanda", 34];
+
+console.log("Subjects:", subjects);
+console.log("Student:", student);
